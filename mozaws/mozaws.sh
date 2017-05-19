2edc22c360d49e8b79d26ba767a32cd1582ffc01
Dave Zeber <dzeber@mozilla.com>
2017-05-18 19:10:54 -0500

    mozaws: Added shell function to list current clusters

--- mozaws/mozaws.sh
+++ mozaws/mozaws.sh
@@ -4,7 +4,7 @@
 
 ## The directory where cluster info will be written.
 ## If this script is sourced on login, it will get picked up by mozaws.init.R.
-MOZAWS_INFO_DIR=~/.mozaws.clus
+export MOZAWS_INFO_DIR=~/.mozaws.clus
 
 ## Alias for ssh that will pull in the custom config with current mozaws clusters.
 ## To override this config (or revert to the standard config), use `-F`.
@@ -22,6 +22,7 @@ alias ssh='mozaws_ssh_config && ssh -F ~/.ssh/config_mozaws_tmp'
 ## The custom config file is written to ~/.ssh/config_mozaws_tmp.
 ## To pull in the config, use `ssh -F ~/.ssh/config_mozaws_tmp`
 mozaws_ssh_config() {
+    ## How many mozaws clusters are listed in the info dir?
     NUM_CLUS=$(ls -1 "$MOZAWS_INFO_DIR" | wc -l)
     if [[ $NUM_CLUS -eq 0 ]]; then
         ## If no clusters are listed, just use the standard config file.
@@ -41,36 +42,27 @@ mozaws_ssh_config() {
         echo "$MOZAWS_CONFIG" > ~/.ssh/config_mozaws_tmp
         cat ~/.ssh/config >> ~/.ssh/config_mozaws_tmp
     fi
+    ## Set the recommended permissions on the new config file, in case they are
+    ## not already set.
     chmod 600 ~/.ssh/config_mozaws_tmp
 }
 
 
-## Attempt to connect to an AWS EMR cluster whose info has been written
-## to the MOZAWS_INFO_DIR by mozaws.
-## Currently this does not allow passing other args to ssh.
-#ssh_mozaws_cluster() {
-#    CLUSTERS=$(ls -1 "$MOZAWS_INFO_DIR")
-#
-#    ## Which cluster should we connect to?
-#    if [[ -n "$1" ]]; then
-#        ## The cluster ID was passed - use that.
-#        CLUSTER_ID=$1
-#    elif [[ -z "$CLUSTERS" ]]; then
-#        echo "No mozaws clusters are listed."
-#        return 1
-#    elif [[ $(echo "$CLUSTERS" | wc -l) -eq 1 ]]; then
-#        CLUSTER_ID="$CLUSTERS"
-#    else
-#        MULT_MSG="Multiple clusters are listed."
-#        MULT_MSG="$MULT_MSG Specify the ID of the cluster to connect to:"
-#        echo "$MULT_MSG"
-#        echo "$CLUSTERS"
-#        return 1
-#    fi
-#
-#    CLUSTER_INFO_PATH="$MOZAWS_INFO_DIR/$CLUSTER_ID"
-#    IP_ADDR=$(cat $CLUSTER_INFO_PATH/dns_name)
-#    echo "Connecting to cluster $CLUSTER_ID ($IP_ADDR):"
-#    ssh $IP_ADDR
-#}
-#
+## List current mozaws clusters.
+mozaws() {
+    CLUSTERS=$(ls -1 "$MOZAWS_INFO_DIR")
+    if [[ $(echo "$CLUSTERS" | wc -w) -eq 0 ]]; then
+        echo "There are no mozaws clusters available."
+        return 0
+    fi
+    echo "Current mozaws clusters:"
+    for CLUSID in $CLUSTERS; do
+        CLUSNAME=$(cat "$MOZAWS_INFO_DIR/$CLUSID/cluster_name")
+        CREATIONTIME=$(cat "$MOZAWS_INFO_DIR/$CLUSID/creation_time")
+        IPADDR=$(cat "$MOZAWS_INFO_DIR/$CLUSID/dns_name")
+        echo
+        echo "Cluster $CLUSID ($CLUSNAME):"
+        echo -e "\t- Created at $CREATIONTIME"
+        echo -e "\t- Address: $IPADDR"
+    done
+}
